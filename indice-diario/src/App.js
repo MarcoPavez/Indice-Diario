4128ef1d9b6b107264f44d683dd3094abad9b580
Marco Pavez <marcopz03@outlook>
2023-04-03 15:19:11 -0400

    Creación proyecto en React

---
+++ indice-diario/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Inicio from "./pages/Inicio"
+/* import Glosario from "./pages/Glosario"
+import Planes from "./pages/Planes" */
+
+
+const Layout = () => {
+  return(
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout/>,
+    children: [
+      {
+        path:"/",
+        element:<Inicio/>
+      },
+      /* {
+        path:"/glosario",
+        element:<Glosario/>
+      },
+      {
+        path:"/planes",
+        element:<Planes/>
+      } */
+    ]
+  }
+])
+
+function App() {
+  return (
+    <>
+
+      <RouterProvider router={router}/>
+
+    </>
+  )
+}
+
+export default App
