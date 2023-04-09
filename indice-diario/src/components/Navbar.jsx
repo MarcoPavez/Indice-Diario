b03c7a1324e74bc0aca55c4987b70a80c2e86b2a
Marco Pavez <marcopz03@outlook>
2023-04-09 03:43:50 -0400

    Cambios estructura y nombres en navbar

--- indice-diario/src/components/Navbar.jsx
+++ indice-diario/src/components/Navbar.jsx
@@ -5,31 +5,46 @@ import indicadores from "../img/indicadores.jpg";
 function Navbar() {
   return (
     <nav>
-      <div class="nav__up">
-        <h1>Índice Diario</h1>
-        <h2>Proveemos la información que necesitas</h2>
-        <p>
-          Desplegamos los valores diarios e históricos de distintos
+      <div class="navSuperior">
+        <div id="iconoBarras">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+          </svg>
+        </div>
+
+        <h1>
+          Índice
           <br />
-          índices del mercado. Comienza a explorar y obtén información
-          <br />
-          valiosa sobre el panorama económico nacional.
-        </p>
-      </div>
-      <div class="nav__down">
+          Diario
+        </h1>
+
         <ul>
           <li>Inicio</li>
           <li>Planes</li>
           <li>Glosario</li>
           <li>Ingresa</li>
         </ul>
+      </div>
 
-        <div class="nav__down__img">
-          <img
-            src={indicadores}
-            alt="Imagen provisoria. Gráfico que visualiza las alzas y caídas de un indicador."
-          />
-        </div>
+      <div class="navInferior">
+        <h2>
+          Proveemos la información
+          <br />
+          que necesitas
+        </h2>
+      </div>
+
+      <p id="navInferiorParrafo">
+        Desplegamos los valores diarios e históricos de distintos{" "}
+        <strong>índices del mercado</strong>. Comienza a explorar y obtén
+        información valiosa sobre el panorama económico nacional.
+      </p>
+
+      <div class="navImagen">
+        <img
+          src={indicadores}
+          alt="Imagen provisoria. Gráfico que visualiza las alzas y caídas de un indicador."
+        />
       </div>
     </nav>
   );
