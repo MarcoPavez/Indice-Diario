471370ade4ea8c6ed87bb0263f069b4903f92c75
Marco Pavez <marcopz03@outlook>
2023-04-29 15:08:03 -0400

    Registro de consultas V.1

---
+++ fastify-cliente/admin-indicediario/pages/consulta.js
@@ -0,0 +1,13 @@
+import BarraNavegacion from "@/components/barraNavegacion";
+import PanelConsultas from "@/components/panelConsultas";
+import PiePagina from "@/components/piePagina";
+
+export default function PaginaConsulta() {
+    return (
+        <>
+            <BarraNavegacion />
+            <PanelConsultas />
+            <PiePagina />
+        </>
+    )
+}
