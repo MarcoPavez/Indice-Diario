211a2869c8c26cc65bf74021bbb9a5db745473da
MarcoPavez <marcopz03@outlook.com>
2023-05-01 21:31:03 -0400

    Estilo navbar

--- fastify-cliente/admin-indicediario/components/barraNavegacion.js
+++ fastify-cliente/admin-indicediario/components/barraNavegacion.js
@@ -2,15 +2,14 @@ export default function BarraNavegacion(props) {
     return (
         <header>
             <nav>
-                <div id="iconoBarras">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                        <path
-                            d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-                    </svg>
-                </div>
+                <picture id="iconoBarras">
+                    <svg id="barras" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z"/></svg>
+                </picture>
+
                 <a href="/inicio.html">
                     <h1>Índice Diario</h1>
                 </a>
+
                 <ul id="listaBarraNavegacion">
                     <li><a href="/inicio.html">Inicio</a></li>
                     <li><a href="/planes.html">Planes</a></li>
@@ -18,6 +17,17 @@ export default function BarraNavegacion(props) {
                     <li><a href="/registro.html">Regístrate</a></li>
                 </ul>
             </nav>
+
+            <section id="menuLateral">
+                <h3>Menú</h3>
+                <ol>
+                    <li><a href="/inicio.html"><img src="./assets/img/casa.svg" alt=""/>Inicio</a></li>
+                    <li><a href="/planes.html"><img src="./assets/img/papel.svg" alt=""/>Planes</a></li>
+                    <li><a href="/glosario.html"><img src="./assets/img/listado.svg" alt=""/>Glosario</a></li>
+                    <li><a href="/ingreso.html"><img src="./assets/img/ingreso.svg" alt=""/>Ingresa</a></li>
+                    <li><a href="/registro.html"><img src="./assets/img/registro.svg" alt=""/>Regístrate</a></li>
+                </ol>
+            </section>
         </header>
     )
 }
